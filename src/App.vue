e1d0ac639e41b175888483a900a44797a46060bd
smetak13 <simon.smejkal@seznam.cz>
2018-12-09 16:56:02 +0100

    herni mechanika komplet funkční

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app container-fluid">
+    <h3 class="text-center"> {{ points }}</h3>
     <div class="game">
       <div class="card" v-for="(card, i) in activeDeck.cards" :key="i">
-        <img v-if="activeCard===i" :src="getFrontImage(i)" @click="flipCard()" width="140" height="140" class="img-fluid">
-        <img v-else :src="getBackImage()" @click="flipCard(i)" width="140" height="140" class="img-fluid">
+        <img v-if="activeCard.card1===i"
+         :src="getFrontImage(i)" width="100" height="100" class="img-fluid">
+        <img v-else-if="activeCard.card2===i"
+         :src="getFrontImage(i)" width="100" height="100" class="img-fluid">
+        <img v-else-if="discardedCards.includes(i)"
+          :src="getEndImage()" width="100" height="100" class="img-fluid">
+        <img v-else
+        :src="getBackImage()" @click="flipCard(i)" width="100" height="100" class="img-fluid">
       </div>
     </div>
   </div>
@@ -20,7 +27,13 @@ export default {
   data() {
     return {
       activeDeck: harryPotterPexeso,
-      activeCard: undefined,
+      activeCard: {
+        card1: undefined,
+        card2: undefined,
+      },
+      points: 0,
+      loading: false,
+      discardedCards: [],
     }
   },
   methods: {
@@ -30,8 +43,37 @@ export default {
     getFrontImage(i) {
       return require('../src/assets/img/' + this.activeDeck.name + '/' + this.activeDeck.cards[i].cardName + '.png');
     },
-    flipCard(i) {
-      this.activeCard = i;
+    getEndImage(i) {
+      return require('../src/assets/img/end.png')
+    },
+    flipCard(index) {
+      if (this.loading) return;
+      if (this.activeCard.card1 === undefined ) {
+        this.activeCard.card1 = index;
+      }
+      else {
+        this.activeCard.card2 = index;
+        if (this.activeCard.card2===this.activeCard.card1) {
+          return;
+        }
+        this.evaluate()
+      }
+    },
+    evaluate() {
+      if (this.activeDeck.cards[this.activeCard.card1].cardName === this.activeDeck.cards[this.activeCard.card2].cardName) {
+        this.points += 1;
+        this.discardedCards.push(this.activeCard.card1);
+        this.discardedCards.push(this.activeCard.card2);
+      }
+      this.loading = true;
+      setTimeout(() => {
+        this.reset();
+      }, 2000);
+    },
+    reset() {
+      this.activeCard.card1 = undefined;
+      this.activeCard.card2 = undefined;
+      this.loading = false;
     }
   },
 }
