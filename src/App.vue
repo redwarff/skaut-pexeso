1c5f00657f8ec6a5d547c610ef2a71bad07b3810
smetak13 <simon.smejkal@seznam.cz>
2018-12-09 18:18:00 +0100

    menu kompletní

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app container-fluid">
-    <h3 class="text-center"> {{ points }}</h3>
-    <div class="game">
-      <div class="card" v-for="(card, i) in activeDeck.cards" :key="i">
-        <img v-if="activeCard.card1===i"
-         :src="getFrontImage(i)" width="100" height="100" class="img-fluid">
-        <img v-else-if="activeCard.card2===i"
-         :src="getFrontImage(i)" width="100" height="100" class="img-fluid">
-        <img v-else-if="discardedCards.includes(i)"
-          :src="getEndImage()" width="100" height="100" class="img-fluid">
-        <img v-else
-        :src="getBackImage()" @click="flipCard(i)" width="100" height="100" class="img-fluid">
+    <div class="menu text-center" v-if="pageSelected === 'menu'">
+      <h1>Choose your name</h1><hr>
+      <h2>Player 1:</h2>
+      <input type="text" v-model="player1.name" placeholder="Write your name here...">
+      <h2>Player 2:</h2>
+      <input type="text" v-model="player2.name" placeholder="Write your name here..."><br><br>
+      <button class="btn btn-lg btn-danger" :disabled="player1.name === undefined || player2.name === undefined"
+        @click="selectPage('game')">Start Game</button>
+    </div>
+    <div class="game" v-if="pageSelected === 'game'">
+      <div class="score text-center">
+        <h3 :class="{'active':isPlayer1Active, 'passive':!isPlayer1Active}"> {{ player1.name }}</h3>
+        <h3>{{ player1.points }} | {{ player2.points }}</h3>
+        <h3 :class="{'active':!isPlayer1Active, 'passive':isPlayer1Active}"> {{ player2.name }} </h3>
+      </div>
+      <div class="game-board">
+        <div class="card" v-for="(card, i) in activeDeck.cards" :key="i">
+          <img v-if="activeCard.card1 === i"
+          :src="getFrontImage(i)" class="img-fluid">
+          <img v-else-if="activeCard.card2 === i"
+          :src="getFrontImage(i)" class="img-fluid">
+          <img v-else-if="discardedCards.includes(i)"
+            :src="getEndImage()" class="img-fluid">
+          <img v-else
+          :src="getBackImage()" @click="flipCard(i)" class="img-fluid">
+        </div>
       </div>
     </div>
+    <div class="result" v-if="pageSelected === 'result'"></div>
   </div>
 </template>
 
@@ -26,17 +42,29 @@ export default {
   },
   data() {
     return {
+      pageSelected: 'menu',
       activeDeck: harryPotterPexeso,
+      isPlayer1Active: true,
       activeCard: {
         card1: undefined,
         card2: undefined,
       },
-      points: 0,
+      player1: {
+        name: undefined,
+        points: 0,
+      },
+      player2: {
+        name: undefined,
+        points: 0,
+      },
       loading: false,
       discardedCards: [],
     }
   },
   methods: {
+    selectPage(page) {
+      this.pageSelected = page;
+    },
     getBackImage() {
       return require('../src/assets/img/back.png');
     },
@@ -61,7 +89,12 @@ export default {
     },
     evaluate() {
       if (this.activeDeck.cards[this.activeCard.card1].cardName === this.activeDeck.cards[this.activeCard.card2].cardName) {
-        this.points += 1;
+        if (this.isPlayer1Active) {
+          this.player1.points += 1;
+        }
+        else {
+          this.player2.points += 1;
+        }
         this.discardedCards.push(this.activeCard.card1);
         this.discardedCards.push(this.activeCard.card2);
       }
@@ -70,7 +103,11 @@ export default {
         this.reset();
       }, 2000);
     },
+    switchPlayers() {
+      this.isPlayer1Active = !this.isPlayer1Active;
+    },
     reset() {
+      this.switchPlayers();
       this.activeCard.card1 = undefined;
       this.activeCard.card2 = undefined;
       this.loading = false;
@@ -80,7 +117,11 @@ export default {
 </script>
 
 <style>
-.game {
+img {
+  width: 90px;
+  height: 90px;
+}
+.game-board {
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-gap: 10px;
@@ -88,5 +129,36 @@ export default {
 }
 .card {
   border: none;
+}
+.active {
+  color: rgb(124, 12, 12);
+  font-weight: bold;
+}
+.passive {
+  color: black;
+  font-weight: normal;
+}
+h3 {
+  display: inline;
+  padding: 0px 20px 0px 20px;
+}
+h2 {
+  margin-top: 30px;
+  margin-bottom: 20px;
+}
+h1 {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+@media (max-width: 800px) {
+  .game-board {
+    grid-template-columns: repeat(6, 1fr);
+  }
+}
+@media (max-width: 600px) {
+  .game-board {
+    grid-template-columns: repeat(4, 1fr);
+  }
 }
 </style>
