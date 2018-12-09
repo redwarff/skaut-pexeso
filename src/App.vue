6dbbcedc5fd8bb761824559ab10e2822fcf3b859
smetak13 <simon.smejkal@seznam.cz>
2018-12-09 15:35:57 +0100

    otaceni jednotlivych karet funkční

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
+  <div id="app container-fluid">
     <div class="game">
       <div class="card" v-for="(card, i) in activeDeck.cards" :key="i">
-        <img :src="getImage(i)">
+        <img v-if="activeCard===i" :src="getFrontImage(i)" @click="flipCard()" width="140" height="140" class="img-fluid">
+        <img v-else :src="getBackImage()" @click="flipCard(i)" width="140" height="140" class="img-fluid">
       </div>
     </div>
   </div>
@@ -19,16 +20,31 @@ export default {
   data() {
     return {
       activeDeck: harryPotterPexeso,
+      activeCard: undefined,
     }
   },
   methods: {
-    getImage(i) {
-      return require('../src/assets/img/' + this.activeDeck.name + '/' + this.activeDeck.cards[i].index + '.png')
+    getBackImage() {
+      return require('../src/assets/img/back.png');
+    },
+    getFrontImage(i) {
+      return require('../src/assets/img/' + this.activeDeck.name + '/' + this.activeDeck.cards[i].cardName + '.png');
+    },
+    flipCard(i) {
+      this.activeCard = i;
     }
-  }
+  },
 }
 </script>
 
 <style>
-
+.game {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-gap: 10px;
+  padding: 10px;
+}
+.card {
+  border: none;
+}
 </style>
--- src/pexeso.js
+++ src/pexeso.js
@@ -1,11 +1,18 @@
 class Pexeso {
 
-    constructor(name, numberOfCards, numberOfRows, numberOfCols, cards) {
+    constructor(name, numberOfCards, numberOfCols) {
         this.name = name;
         this.numberOfCards = numberOfCards;
-        this.numberOfRows = numberOfRows;
         this.numberOfCols = numberOfCols;
-        this.cards = cards;
+        this.cards = [];
+    }
+
+    createCards() {
+        for (let i = 1; i <= this.numberOfCards / 2; i++) {
+            this.cards.push({
+                cardName: i
+            })
+        }
     }
 
     makePairs() {
@@ -15,127 +22,18 @@ class Pexeso {
         });
     }
 
-    shuffleDeck(array) {
-        for (let i = array.length - 1; i > 0; i--) {
+    shuffleDeck() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]]
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]
         }
     }
 }
 
-const harryPotterPexeso = new Pexeso('harryPotter', 56, 7, 8, [
-    {
-        index: 1,
-        src: 'harryPotter/1.png'
-    },
-    {
-        index: 2,
-        src: '/src/assets/img/harryPotter/2.png'
-    },
-    {
-        index: 3,
-        src: './src/assets/img/harry_potter/3.png'
-    },
-    {
-        index: 4,
-        src: './src/assets/img/harry_potter/4.png'
-    },
-    {
-        index: 5,
-        src: './src/assets/img/harry_potter/5.png'
-    },
-    {
-        index: 6,
-        src: './src/assets/img/harry_potter/6.png'
-    },
-    {
-        index: 7,
-        src: './src/assets/img/harry_potter/7.png'
-    },
-    {
-        index: 8,
-        src: './src/assets/img/harry_potter/8.png'
-    },
-    {
-        index: 9,
-        src: './src/assets/img/harry_potter/9.png'
-    },
-    {
-        index: 10,
-        src: './src/assets/img/harry_potter/10.png'
-    },
-    {
-        index: 11,
-        src: './src/assets/img/harry_potter/11.png'
-    },
-    {
-        index: 12,
-        src: './src/assets/img/harry_potter/12.png'
-    },
-    {
-        index: 13,
-        src: './src/assets/img/harry_potter/13.png'
-    },
-    {
-        index: 14,
-        src: './src/assets/img/harry_potter/14.png'
-    },
-    {
-        index: 15,
-        src: './src/assets/img/harry_potter/15.png'
-    },
-    {
-        index: 16,
-        src: './src/assets/img/harry_potter/16.png'
-    },
-    {
-        index: 17,
-        src: './src/assets/img/harry_potter/17.png'
-    },
-    {
-        index: 18,
-        src: './src/assets/img/harry_potter/18.png'
-    },
-    {
-        index: 19,
-        src: './src/assets/img/harry_potter/19.png'
-    },
-    {
-        index: 20,
-        src: './src/assets/img/harry_potter/20.png'
-    },
-    {
-        index: 21,
-        src: './src/assets/img/harry_potter/21.png'
-    },
-    {
-        index: 22,
-        src: './src/assets/img/harry_potter/22.png'
-    },
-    {
-        index: 23,
-        src: './src/assets/img/harry_potter/23.png'
-    },
-    {
-        index: 24,
-        src: './src/assets/img/harry_potter/24.png'
-    },
-    {
-        index: 25,
-        src: './src/assets/img/harry_potter/25.png'
-    },
-    {
-        index: 26,
-        src: './src/assets/img/harry_potter/26.png'
-    },
-    {
-        index: 27,
-        src: './src/assets/img/harry_potter/27.png'
-    },
-    {
-        index: 28,
-        src: './src/assets/img/harry_potter/28.png'
-    }
-]);
+const harryPotterPexeso = new Pexeso('harryPotter', 56, 8);
+
+harryPotterPexeso.createCards();
+harryPotterPexeso.makePairs();
+harryPotterPexeso.shuffleDeck();
 
 export default harryPotterPexeso;
