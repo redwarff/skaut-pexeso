18a6b2fd7e98d430ff8f66a2837a281263fa8477
smetak13 <simon.smejkal@seznam.cz>
2018-12-10 16:15:43 +0100

    pridana podminka pro remizu

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -2,7 +2,10 @@
   <div class="result text-center" v-if="pageSelected === 'result'">
     <h1>Result</h1><hr>
     <h4 v-if="player1.score > player2.score">{{ player1.name }} won with a total of {{ player1.score }} points.</h4>
-    <h4 v-else> {{ player2.name }} won with a total of {{ player2.score }} points.</h4><br><br>
+    <h4 v-else-if="player2.score > player1.score"> {{ player2.name }} won with a total of {{ player2.score }} points.</h4>
+    <h4 v-else> Match ended as a draw.</h4>
+    
+    <br><br>
     <button class="btn btn-lg btn-danger" @click="newGame()">Start a new game</button>
   </div>
 </template>
